e9b62a0cae463126b56b948c82b0aaa9886e5b27
priyankaMunaga <munagapriyanka9@gmail.com>
2023-11-01 13:19:16 +0530

    cucumber steps for parabank login

---
+++ src/test/java/org/example/stepDefinition/paraBankLogin.java
@@ -0,0 +1,27 @@
+package org.example.stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Sleeper;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class paraBankLogin {
+    @Given("user should  be on login page")
+    public void method2() {
+        open("www.google.com");
+     sleep(6000);
+    }
+
+    @When("user will enter the wrong username and password")
+    public void userWillEnterTheWrongUsernameAndPassword() {
+        $(By.xpath("//input[@name='username']")).sendKeys("priyankamunaga");
+       $(By.xpath("//input[@name='password']")).sendKeys("14567");
+        $(By.xpath("//input[@name='password']")).sendKeys("14567", Keys.ENTER);
+        //$(By.xpath("//p[@class='error']"))
+    }
+}
+
+
---
+++ src/test/resources/features/parabank.feature
@@ -0,0 +1,10 @@
+Feature: login for para bank
+  Scenario: para bank login scenario
+    Given user should  be on login page
+   When user will enter the wrong username and password
+   # And user will click the next button
+   # Then the user get the error
+
+
+
+
