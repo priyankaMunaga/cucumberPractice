59a6765c9d73698a574a72ebea36e60e778c7afc
priyankaMunaga <munagapriyanka9@gmail.com>
2024-02-15 13:07:28 +0530

    changes done.

--- Assignment/src/test/java/com/test/assignment/steps/firstAssignmentSteps.java
+++ Assignment/src/test/java/com/test/assignment/steps/firstAssignmentSteps.java
@@ -14,9 +14,7 @@ public class firstAssignmentSteps {
     public void userNavigateToTheQaTechHubPage() {
         System.out.println("starting the web browser");
         open("https://qatechhub.com");
-        System.out.println("opening the qa tech hub page ");
-        //sleep(40000);
-        $(By.xpath("//h3[contains(text(),'QA Tech Hub: QA Automation Tools Trainings and Tut')]"));
+        $(By.xpath("//h3[contains(text(),'QA Tech Hub: QA Automation Tools Trainings and Tut')]")).click();
     }
 
     @Then("Print the page title")
@@ -26,32 +24,27 @@ public class firstAssignmentSteps {
     @When("the user navigate to the facebook page")
     public void theUserNavigateToTheFacebookPage() {
         open("https://www.facebook.com/");
-       // sleep(3000);
     }
 
     @Then("user navigate to the back page")
     public void userNavigateToTheBackPage() {
         Selenide.back();
-        //sleep(3000);
 
     }
 
     @Then("Print the URL of the current page")
     public void printTheURLOfTheCurrentPage() {
         String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
-       // sleep(3000);
     }
 
     @When("the user navigate to the forward page")
     public void theUserNavigateToTheForwardPage() {
         Selenide.forward();
-        sleep(3000);
     }
 
     @Then("the user Reload the page")
     public void theUserReloadThePage() {
         Selenide.refresh();
-        sleep(3000);
 
     }
 
