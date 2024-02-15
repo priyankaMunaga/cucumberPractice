45b5d1245d4275ea44622cdbc90999e6c2d2ae90
priyankaMunaga <munagapriyanka9@gmail.com>
2024-02-15 15:56:00 +0530

    added one more validation for current url

--- Assignment/src/test/java/com/test/assignment/steps/firstAssignmentSteps.java
+++ Assignment/src/test/java/com/test/assignment/steps/firstAssignmentSteps.java
@@ -1,10 +1,13 @@
 package com.test.assignment.steps;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -12,13 +15,17 @@ import static com.codeborne.selenide.Selenide.*;
 public class firstAssignmentSteps {
     @Given("user navigate to the qaTechHub page")
     public void userNavigateToTheQaTechHubPage() {
-        System.out.println("starting the web browser");
+
         open("https://qatechhub.com");
-        $(By.xpath("//h3[contains(text(),'QA Tech Hub: QA Automation Tools Trainings and Tut')]")).click();
     }
 
     @Then("Print the page title")
     public void printThePageTitle() {
+        String currentPageTitle = title();
+        Assert.assertEquals(currentPageTitle, "QA Automation Tools Trainings and Tutorials | QA Tech Hub");
+        System.out.println("current page title  :" + currentPageTitle);
+
+
     }
 
     @When("the user navigate to the facebook page")
@@ -35,6 +42,9 @@ public class firstAssignmentSteps {
     @Then("Print the URL of the current page")
     public void printTheURLOfTheCurrentPage() {
         String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        System.out.println("current Url :" + currentUrl);
+        Assert.assertTrue(currentUrl.contains("https://qatechhub.com"));
+
     }
 
     @When("the user navigate to the forward page")
