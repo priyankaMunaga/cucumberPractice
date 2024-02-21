3a6a81a1a9dcfa0295f5df86f13793e3375ec928
vishnukode <vishnu.kode@impact.com>
2024-02-21 16:42:31 +0530

    adding third assigmnet using testNG

---
+++ PageObject/src/test/java/qaAssaignments/ThirdAssignment.java
@@ -0,0 +1,37 @@
+package qaAssaignments;
+
+import org.example.ConfigManager;
+import org.example.Page;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ThirdAssignment {
+
+static String flipCartUrl= ConfigManager.getProperty("flipCartUri");
+
+
+    @Before
+    public void testSetUp(){
+        Page.SetUp("chrome");
+    }
+    @Test
+    public void validateTheLinksOnFlipCart(){
+        Page.driver.get(flipCartUrl);
+      List<WebElement> webLinks= Page.driver.findElements(By.tagName("a"));
+      for (WebElement link:webLinks){
+          System.out.println(link.getSize());
+          System.out.println(link.getText() + " - " + link.getAttribute("href"));
+
+      }
+
+    }
+    @After
+    public void tearDown(){
+        Page.closeDriver();
+    }
+}
