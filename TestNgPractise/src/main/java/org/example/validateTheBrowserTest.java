215b6ebc3a34a62fd2dc246d85ba0b53d78d6288
priyankaMunaga <munagapriyanka9@gmail.com>
2024-07-03 11:29:46 +0200

    adding new module

---
+++ TestNgPractise/src/main/java/org/example/validateTheBrowserTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class validateTheBrowserTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver= new ChromeDriver();
+
+    }
+
+}
