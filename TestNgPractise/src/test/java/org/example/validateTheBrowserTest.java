4b081e4f566f7d6c5f502297f5991487bc7c9975
priyankaMunaga <munagapriyanka9@gmail.com>
2024-07-03 11:59:03 +0200

    moved from main to test path

---
+++ TestNgPractise/src/test/java/org/example/validateTheBrowserTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class validateTheBrowserTest {
+
+    @BeforeTest
+    public void testSetUp(){
+
+    }
+
+
+    @Test
+    public static void firstTest() {
+
+    }
+}
